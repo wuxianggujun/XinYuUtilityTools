ba4839b09de9a9e06ca3b58f22c954362851306e
wuxianggujun <3344207732@qq.com>
2024-06-16 13:14:45 +0800

    添加SQL模块

--- include/mainwindow.h
+++ include/mainwindow.h
@@ -1,17 +1,15 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
-
 #include <QStandardItemModel>
-#include <QtCore/QVariant>
-#include <QtWidgets/QApplication>
-#include <QtWidgets/QHBoxLayout>
-#include <QtWidgets/QMainWindow>
-#include <QtWidgets/QWidget>
+#include <QVariant>
+#include <QApplication>
+#include <QHBoxLayout>
+#include <QWidget>
 #include <QMainWindow>
 #include <QDebug>
 #include <QtCore>
-#include <QtNetwork/QtNetwork>
+#include <QtNetwork>
 #include <QMenuBar>
 #include <QToolBar>
 #include <QStatusBar>
@@ -21,22 +19,32 @@
 #include <QLineEdit>
 #include <QSplitter>
 #include <QListView>
+#include <QTextEdit>
+#include <QTreeView>
 
+#include <QSqlDatabase>
+#include <QSqlQuery>
+#include <QSqlRecord>
 
-class MainWindow : public QMainWindow
-{
-    Q_OBJECT
+class MainWindow : public QMainWindow {
+Q_OBJECT
 
 public:
     explicit MainWindow(QWidget *parent = nullptr);
-    ~MainWindow() override;
 
+    ~MainWindow() override;
+private:
+     void initialize();
 private:
     QWidget *centralWidget;
     QMenuBar *mainMenuBar;
     QSplitter *mainSplitter;
     QVBoxLayout *mainVBoxLayout;
 private slots:
+
     void on_actionOpen_triggered();
+
+    void on_listView_clicked(const QModelIndex &index);
 };
+
 #endif // MAINWINDOW_H
--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,11 +1,38 @@
 
-#include <QTextEdit>
-#include <QTreeView>
 #include "mainwindow.h"
 
 MainWindow::MainWindow(QWidget *parent)
         : QMainWindow(parent) {
 
+    initialize();
+
+    // 连接信号槽
+    QMetaObject::connectSlotsByName(this);
+}
+
+MainWindow::~MainWindow() {
+        delete centralWidget;
+}
+
+
+void MainWindow::on_actionOpen_triggered() {
+    QString curPath = QDir::currentPath();
+    QString fileName = QFileDialog::getOpenFileName(this, tr("打开文件"), curPath, tr("所有文件 (*)"));
+    if (!fileName.isEmpty()) {
+        QFile file(fileName);
+        if (file.open(QIODevice::ReadOnly)) {
+            QMessageBox::information(this, tr("文件操作"), tr("文件打开成功！"));
+        }
+    }
+
+}
+
+void MainWindow::on_listView_clicked(const QModelIndex &index) {
+    QString text = index.data().toString();
+    QMessageBox::information(this, tr("列表点击"), tr("你点击了：") + text);
+}
+
+void MainWindow::initialize() {
     if (objectName().isEmpty()) {
         setObjectName("MainWindow");
     }
@@ -55,29 +82,24 @@ MainWindow::MainWindow(QWidget *parent)
     centralWidget->setLayout(mainVBoxLayout);
 
 
+    QStringList stringList;
+    stringList << "Item1" << "Item2" << "Item3";
+
+    auto *stringListModel = new QStringListModel();
+    stringListModel->setStringList(stringList);
+    listview->setModel(stringListModel);
+    listview->setSelectionMode(QAbstractItemView::SingleSelection);
+    //设置列表不能编辑
+    listview->setEditTriggers(QAbstractItemView::NoEditTriggers);
+    //设置列表不能被睡意拖动
+    listview->setMovement(QListView::Static);
+    //设置列表间隔
+    listview->setSpacing(10);
+
+
+    connect(listview,&QListView::clicked,this,&MainWindow::on_listView_clicked,Qt::DirectConnection);
 
     connect(pOpenAction, &QAction::triggered, this, &MainWindow::on_actionOpen_triggered);
     // 连接槽函数
     connect(pExitAction, &QAction::triggered, this, &QMainWindow::close);
-
-    // 连接信号槽
-    QMetaObject::connectSlotsByName(this);
-}
-
-MainWindow::~MainWindow() {
-    if (centralWidget)
-        delete centralWidget;
-}
-
-
-void MainWindow::on_actionOpen_triggered() {
-    QString curPath = QDir::currentPath();
-    QString fileName = QFileDialog::getOpenFileName(this, tr("打开文件"), curPath, tr("所有文件 (*)"));
-    if (!fileName.isEmpty()) {
-        QFile file(fileName);
-        if (file.open(QIODevice::ReadOnly)) {
-            QMessageBox::information(this, tr("文件操作"), tr("文件打开成功！"));
-        }
-    }
-
 }
